ff24aa4cef6c1f09efb0d4727384987abe72ad6b
Sunil <135955149+esitorsunil@users.noreply.github.com>
2024-11-03 23:55:43 +0530

    Add files via upload

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -12,6 +12,14 @@ export const API_OPTION = {
     }
   };
 
+  export const API_OPTIONS = {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        Authorization: env.process.REACT_APP_TMDB,
+      }
+    };
+
   export const IMG_CDN_URL = "https://image.tmdb.org/t/p/w500";
 
   export const BG_URL = "https://assets.nflxext.com/ffe/siteui/vlv3/47c2bc92-5a2a-4f33-8f91-4314e9e62ef1/web/IN-en-20240916-TRIFECTA-perspective_72df5d07-cf3f-4530-9afd-8f1d92d7f1a8_small.jpg" 
@@ -21,3 +29,5 @@ export const API_OPTION = {
     {identifier:"tamil", name: "Tamil"},
     {identifier:"french", name: "French"},
   ];
+
+  export const OPENAI_KEY = env.process.REACT_APP_OPENAI_KEY
--- src/utils/movieSlice.js
+++ src/utils/movieSlice.js
@@ -4,6 +4,7 @@ const movieSlice = createSlice({
     name: "movies",
     initialState: {
         nowPlayingMovies: null,
+        popularMovies: null,
         trailerVideo: null,
     },
     reducers: {
@@ -13,11 +14,17 @@ const movieSlice = createSlice({
         addPopularMovies: (state,action) => {
             state.popularMovies = action.payload;
         },
+        addTopRatedMovies: (state,action) => {
+            state.topRatedMovies = action.payload;
+        },
+        addUpcomingMovies: (state,action) => {
+            state.upcomingMovies = action.payload;
+        },
         addTrailerVideo: (state,action) => {
             state.trailerVideo = action.payload;
         },
     },
 });
-export const{addNowPlayingMovies , addTrailerVideo, addPopularMovies} = movieSlice.actions;
+export const{addNowPlayingMovies , addTrailerVideo, addPopularMovies , addTopRatedMovies, addUpcomingMovies} = movieSlice.actions;
 
 export default movieSlice.reducer;
