9e312f57f8ee207590f5e61b2ffd57f7abfb7525
Sunil <135955149+esitorsunil@users.noreply.github.com>
2024-11-03 23:45:19 +0530

    Add files via upload
    hooks of popular, top rated movies

--- src/hooks/usePopularMovies.js
+++ src/hooks/usePopularMovies.js
@@ -1,4 +1,4 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { addPopularMovies } from "../utils/movieSlice";
 import { useEffect } from "react";
 import { API_OPTION } from "../utils/constants";
@@ -7,6 +7,9 @@ import { API_OPTION } from "../utils/constants";
 const usePopularMovies = () => {
     //Fetch data from TMDB API and update store
   const dispatch = useDispatch();
+
+  const popularMovies = useSelector(
+    (store) => store.movies.popularMovies);
 
   const getPopularMovies = async () => {
     const data = await fetch(
@@ -17,7 +20,7 @@ const usePopularMovies = () => {
     dispatch(addPopularMovies(json.results));
   };
   useEffect(() => {
-    getPopularMovies();
+   !popularMovies && getPopularMovies();
   },[]);
 
 };
